f9d0b66fe92d8ca075483c622f4711beea6366e3
mohamad <msiysinyuy@gmail.com>
2021-12-19 18:43:50 +0100

    update on the index page

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -4,3 +4,12 @@ export {default as img3} from './project-2.png'
 export {default as img4} from './project-3.png'
 export {default as logo} from './suber_logo1.png'
 export {default as profile} from './me.webp'
+
+// export for  geneosocial welcome clearTimeout
+
+export {default as gs1} from './geneosocial/1.png'
+export {default as gs2} from './geneosocial/2.jpg'
+export {default as gs3} from './geneosocial/3.png'
+export {default as gs4} from './geneosocial/4.jpg'
+export {default as gs5} from './geneosocial/5.jpg'
+export {default as gs6} from './geneosocial/6.gif'
