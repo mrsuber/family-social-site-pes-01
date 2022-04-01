c3509ef3a9a8f2d2361fcf9ca11509e8693d2364
mohamad <msiysinyuy@gmail.com>
2022-04-01 12:28:36 +0100

    modified structure

--- client/src/components/admin/application/Structure.jsx
+++ client/src/components/admin/application/Structure.jsx
@@ -3,7 +3,8 @@ import './styles.css'
 const Structure = ({companyName,cardData,strutureData,setOnStructuralDetail}) => {
 
     const[filteredCardData, setfilteredCardData] = useState([])
-    const[filteredStructuralData,setFilteredStructuralData] = useState([])
+    const[filteredStructuralData,setFilteredStructuralData] = useState(null)
+      const [lenght,setLenth] = useState(50)
 
   useEffect(()=>{
     const data = (cardData)=>{
@@ -29,9 +30,44 @@ const Structure = ({companyName,cardData,strutureData,setOnStructuralDetail}) =>
       }
       return
     }
+
+    const filteredStructuralDataCompanyLevel1Lenght =(data)=>{
+      if((data && data.companyLevel1.length *20)> lenght ){
+        setLenth(data.companyLevel1.length *20)
+      }
+    }
+
+    const filteredStructuralDataCompanyLevel1PeopleLenght =(data)=>{
+        console.log("i see hear")
+        console.log(data)
+      if(data!==null){
+
+        for(let i=0;i<data.companyLevel1.length; i++){
+
+          if((data.companyLevel1[i].people.length*20)>lenght){
+            setLenth(data.companyLevel1[i].people.length*20)
+
+          }
+        }
+      }
+    }
+
+    filteredStructuralDataCompanyLevel1Lenght(filteredStructuralData)
+    filteredStructuralDataCompanyLevel1PeopleLenght(filteredStructuralData)
+
+
+
     data2(strutureData)
     data(cardData)
-  },[])
+
+
+
+  },[companyName,cardData,filteredCardData,filteredStructuralData,strutureData,lenght])
+
+
+console.log(lenght)
+
+
 
 
 
@@ -46,7 +82,7 @@ const Structure = ({companyName,cardData,strutureData,setOnStructuralDetail}) =>
           <h3 className="admin__section-head">Research before application</h3>
           <div className="admin__graph-content_apply ">
 
-      <div className="admin__graph-board_apply">
+      <div className="admin__graph-board_apply" style={{width:`${lenght}rem`}}>
 data
       <div className="admin__app_tree">
           <ul>
@@ -57,7 +93,7 @@ data
 
                         <div className="AdminStructure__company-mainItem">
                           <h3>Name: <span>{filteredCardData.companyName}</span></h3>
-                          <h3>Location: <span>{filteredStructuralData.companyHeadOfficeLocation}</span></h3>
+                          <h3>Location: <span>{  filteredStructuralData && filteredStructuralData.companyHeadOfficeLocation}</span></h3>
 
                         </div>
                         <span className="AdminStructure__hoverSpan">more...</span>
@@ -66,34 +102,45 @@ data
 
                   <ul>
                     {
-                      filteredStructuralData.companyLevel1.map(()=>(
-                        <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Grand Child in loop</span></a>
+                      filteredStructuralData && filteredStructuralData.companyLevel1.map((data,i)=>(
+                        <li key={i}>
+                            <a href="">
+
+                                <div className="AdminStructure__company-logo">
+                                  <img src={filteredCardData.logo} alt="imgchild"/>
+                                </div>
+
+                                <div className="AdminStructure__company-mainItem">
+                                  <h3>Name: <span>{data.name}</span></h3>
+
+                                </div>
+                                <span className="AdminStructure__hoverSpan">more...</span>
+
+                            </a>
                           <ul>
-                            <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Great Grand Child loop</span></a></li>
-                            <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Great Grand Child loop</span></a></li>
-                          </ul>
+                          {
+                            data.people.map((item,i2)=>(
+                              <li key={i2}>
+                                <a href="">
+                                <div className="AdminStructure__company-logo">
+                                  <img src={item.photo} alt="imgchild"/>
+                                </div>
+
+                                <div className="AdminStructure__company-mainItem">
+                                  <h3>Name: <span>{item.name}</span></h3>
+
+
+                                </div>
+                                <span className="AdminStructure__hoverSpan">more...</span>
+                                </a></li>
+
+                            ))
+                          }
+                            </ul>
                         </li>
                       ))
                     }
-                      <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Grand Child</span></a>
-                        <ul>
-                          <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Great Grand Child</span></a></li>
-                          <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Great Grand Child</span></a></li>
-                        </ul>
-                      </li>
-
-
-                      <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Grand Child</span></a>
-                        <ul>
-                          <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Great Grand Child</span></a></li>
-                          <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Great Grand Child</span></a></li>
-                          <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Great Grand Child</span></a></li>
-                        </ul>
-                      </li>
-
-
-                      <li><a href=""><img src={filteredCardData.logo} alt="imggrandchild"/><span className="AdminStructure__hoverSpan">Grand Child</span></a></li>
-                  </ul>
+                    </ul>
               </li>
           </ul>
       </div>
--- client/src/data/companySturcturalData.js
+++ client/src/data/companySturcturalData.js
@@ -1,5 +1,4 @@
 
-import logo3 from '../images/admin/Oracle_Logo.png'
 export const companyStructuralData = [
   // {
   //   companyName:"Google",
@@ -18,15 +17,51 @@ export const companyStructuralData = [
             photo:'https://customsitesmedia.usc.edu/wp-content/uploads/sites/59/2016/02/16125644/Ellison_1200x800-824x549.jpg',
             title:'Chairman of the Board and Chief Technology Officer',
             Biography:'Larry Ellison is chairman of Oracle Corporation and chief technology officer. He founded the company in 1977 and served as CEO until September 2014. He also races sailboats, flies planes, and plays tennis and guitar.',
-
+            source:[
+              {
+                name:"Oracle",
+                link:"https://www.oracle.com/sa/corporate/executives/"
+              }
+            ]
           },
           {
             name:'Safra A. Catz',
             photo:'https://www.gsb.stanford.edu/sites/default/files/styles/270_x_270/public/person_photos/catz-safra-a_0.jpg?itok=obMspJYv',
             title:'Chief Executive Officer',
             Biography:'Safra A. Catz has served as chief executive officer of Oracle Corporation since 2014 and a member of the company’s board of directors since 2001. She previously served as president of Oracle and has also served as the company’s chief financial officer. Prior to being named president, Catz held various other positions since starting at Oracle in 1999. She currently serves as a director of The Walt Disney Company and previously served as a director of HSBC Holdings plc',
-
+            source:[
+              {
+                name:"Oracle",
+                link:"https://www.oracle.com/sa/corporate/executives/"
+              }
+            ]
           },
+          {
+            name:'Dorian Daley',
+            photo:'https://s3.amazonaws.com/spoke-profiles-prod-assets/avatars/210x210h/9d95919f5e9b7504dff44ab99a2d37ac0463cfbb.png',
+            title:'Executive Vice President and General Counsel',
+            Biography:'Dorian Daley serves as executive vice president and general counsel of Oracle Corporation. She began her career at Oracle in 1992 after spending five years with the commercial litigation group of Landels, Ripley & Diamond in San Francisco. She is a 1986 graduate of the Santa Clara University School of Law and a 1981 graduate of Stanford University. Prior to her appointment as general counsel, Daley was a vice president and associate general counsel for Oracle.',
+            source:[
+              {
+                name:"Oracle",
+                link:"https://www.oracle.com/sa/corporate/executives/"
+              }
+            ]
+          },
+
+          {
+            name:'Edward Screven',
+            photo:'https://cdn.theorg.com/120898f1-4e9e-48da-bb00-34c99c6a01be_medium.png',
+            title:'Chief Corporate Architect',
+            Biography:"Edward Screven is chief corporate architect at Oracle. Reporting to Executive Chairman and Chief Technical Officer Larry Ellison, he drives technology and architecture decisions across all Oracle products to ensure that product development is consistent with Oracle's overall strategy. An Oracle veteran since 1986, he is responsible for Oracle's open-source businesses, including Linux, virtualization, and MySQL. Screven also leads company-wide strategic initiatives, including industry standards and security.",
+            source:[
+              {
+                name:"Oracle",
+                link:"https://www.oracle.com/sa/corporate/executives/"
+              }
+            ]
+          },
+
         ]
       },
 
@@ -38,15 +73,182 @@ export const companyStructuralData = [
             photo:'https://customsitesmedia.usc.edu/wp-content/uploads/sites/59/2016/02/16125644/Ellison_1200x800-824x549.jpg',
             title:'Chairman of the Board and Chief Technology Officer',
             Biography:'Larry Ellison is chairman of Oracle Corporation and chief technology officer. He founded the company in 1977 and served as CEO until September 2014. He also races sailboats, flies planes, and plays tennis and guitar.',
-
+            source:[
+              {
+                name:"Oracle",
+                link:"https://www.oracle.com/sa/corporate/executives/"
+              }
+            ]
           },
           {
             name:'Safra A. Catz',
             photo:'https://www.gsb.stanford.edu/sites/default/files/styles/270_x_270/public/person_photos/catz-safra-a_0.jpg?itok=obMspJYv',
             title:'Chief Executive Officer',
             Biography:'Safra A. Catz has served as chief executive officer of Oracle Corporation since 2014 and a member of the company’s board of directors since 2001. She previously served as president of Oracle and has also served as the company’s chief financial officer. Prior to being named president, Catz held various other positions since starting at Oracle in 1999. She currently serves as a director of The Walt Disney Company and previously served as a director of HSBC Holdings plc',
-
+            source:[
+              {
+                name:"Oracle",
+                link:"https://www.oracle.com/sa/corporate/executives/"
+              }
+            ]
           },
+          {
+            name:'Jeffrey O. Henley',
+            photo:'https://cdn.theorg.com/d0466e43-024e-4683-b0ed-5ac649e8f37d_small.jpg',
+            title:'Vice Chairman of the Board',
+            Biography:"Jeffrey O. Henley is vice chairman of Oracle Corporation. He served as Oracle’s chief financial officer and an executive vice president from 1991 to 2004, and has been a member of Oracle’s board of directors since 1995. Henley was chairman of Oracle from 2004 until 2014. He also serves on Oracle’s Executive Management Committee. Prior to joining Oracle in 1991, Henley served as executive vice president and chief financial officer at Pacific Holding Company, a privately held company with diversified interests in manufacturing and real estate, and as executive vice president and chief financial officer at Saga Corporation, a multibillion-dollar food service company. Henley is chair for the Institute for Energy Efficiency at UC Santa Barbara. He also serves on the advisory board for the Dean of Engineering at the university. Henley has a BA in economics from the University of California at Santa Barbara and an MBA in finance from UCLA.",
+            source:[
+              {
+                name:"Oracle",
+                link:"https://www.oracle.com/sa/corporate/executives/"
+              }
+            ]
+          },
+
+          {
+            name:'Jeffrey S. Berg',
+            photo:'https://assets.boardroominsiders.com/store/b8e299efc6b06aa94a31c7b9f4611c875633db171ce528991d9269b43e49',
+            title:'Chairman of Northside Services, LLC and Former CEO of International Creative Management, Inc',
+            Biography:"Jeffrey S. Berg has been an agent in the entertainment industry for more than 40 years. Berg has served as chairman of Northside Services, LLC, a media and entertainment advisory firm, since 2015. He was chairman of Resolution, a talent and literary agency he founded, from 2013 until 2015. Between 1985 and 2012, Berg was the chairman and CEO of International Creative Management, Inc. (ICM), a talent agency for the entertainment industry. He has served as cochair of California’s Council on Information Technology and was president of the Executive Board of the College of Letters and Sciences at the University of California at Berkeley. He previously served on the Board of Trustees of the Anderson School of Management at the University of California at Los Angeles.",
+            source:[
+              {
+                name:"Oracle",
+                link:"https://www.oracle.com/sa/corporate/executives/"
+              }
+            ]
+          },
+          {
+          name:'Michael J. Boskin',
+          photo:'https://www.longarc.com/wp-content/uploads/2020/08/Michael-Boskin.jpg',
+          title:'Professor of Economics and Hoover Institution Senior Fellow at Stanford University',
+          Biography:"Michael J. Boskin is the Tully M. Friedman Professor of Economics and Wohlford Family Hoover Institution Senior Fellow at Stanford University, where he has been on the faculty since 1971. He is CEO and president of consulting firm Boskin & Co., Inc. Boskin was chairman of the President’s Council of Economic Advisers from 1989 until 1993. He currently serves as a director of Bloom Energy Corporation and previously served as a director of ExxonMobil.",
+          source:[
+            {
+              name:"Oracle",
+              link:"https://www.oracle.com/sa/corporate/executives/"
+            }
+          ]
+        },
+
+        {
+        name:'Bruce R. Chizen',
+        photo:'https://images.forbes.com/media/lists/12/2007/BY3B.jpg',
+        title:'Senior Adviser to Permira Advisers and Former CEO of Adobe Systems Incorporated',
+        Biography:"Bruce R. Chizen is currently an independent consultant and has served as senior adviser to Permira Advisers LLP since 2008 and as a venture partner at Voyager Capital since 2009. He has also served as an operating partner at private equity fund Permira Growth Opportunities since June 2018. Chizen served as a strategic adviser to Adobe Systems Incorporated, a provider of design, imaging, and publishing software for print, internet, and dynamic media production, from 2007 to 2008. From 2000 to 2007, he served as Adobe’s CEO and as its president from 2000 to 2005. He also served as Adobe’s acting CFO from 2006 to 2007; he had joined the company in 1994. Chizen also currently serves as a director of Synopsys, Inc., ChargePoint, Inc., and Informatica.",
+        source:[
+          {
+            name:"Oracle",
+            link:"https://www.oracle.com/sa/corporate/executives/"
+          }
+        ]
+      },
+
+      {
+      name:'George H. Conrades',
+      photo:'https://www.charterworld.com/news/wp-content/uploads/2013/01/George-Conrades.jpg',
+      title:'Senior Advisor to and Former CEO of Akamai Technologies, Inc.',
+      Biography:"George H. Conrades has served as an executive advisor to Akamai Technologies, Inc., a content delivery network services provider for media and software delivery and cloud security solutions, since June 2018. He previously served as Akamai’s CEO from 1999 to 2005 and chairman from 1999 to March 2018. Conrades currently serves as a managing partner at Longfellow Venture Partners, a private venture fund advising and investing in early stage healthcare and technology companies. He also served as a venture partner at Polaris Venture Partners, an early-stage investment company, from 1998 to 2012 and is now a Partner Emeritus. He served as chairman and CEO of Akamai Technologies from 1999 to 2005. Conrades currently serves as a director of Cyclerion Therapeutics, Inc. and previously was a director of Akamai Technologies, Inc., Harley-Davidson, Inc., and Ironwood Pharmaceuticals, Inc.",
+      source:[
+        {
+          name:"Oracle",
+          link:"https://www.oracle.com/sa/corporate/executives/"
+        }
+      ]
+      },
+
+    {
+      name:'Rona A. Fairhead',
+      photo:'https://upload.wikimedia.org/wikipedia/commons/1/18/RonaFairhead.jpg',
+      title:'Former Minister of State, United Kingdom Department for International Trade and Former Chair of the British Broadcasting Corporation Trust',
+      Biography:"Rona A. Fairhead most recently served as Minister of State for Trade and Export Promotion for the United Kingdom Department for International Trade from September 2017 to May 2019. Previously, Fairhead served as chair of the British Broadcasting Corporation Trust (BBC) from 2014 to 2017. From 2006 to 2013, she was chair and chief executive officer of the Financial Times Group Limited, which was a division of Pearson plc, and, prior to that, she served as Pearson’s chief financial officer. Before joining Pearson, Fairhead held a variety of leadership positions at Bombardier Inc. and Imperial Chemical Industries plc. Fairhead serves as a director of Electrocomponents plc. She has previously served as a director of HSBC Holdings plc and PepsiCo, Inc. She is a member of the UK House of Lords.",
+      source:[
+        {
+          name:"Oracle",
+          link:"https://www.oracle.com/sa/corporate/executives/"
+        }
+      ]
+    },
+
+    {
+      name:'Renée J. James',
+      photo:'https://media-exp1.licdn.com/dms/image/C4D03AQET3F7zKG4O9A/profile-displayphoto-shrink_200_200/0/1586532253936?e=1651104000&v=beta&t=ltKpraArYoHkzSna4dJhXePpMSJD5jocSTxkse0ftIY',
+      title:'Chairman and CEO of Ampere Computing, Operating Executive at The Carlyle Group, and Former President of Intel Corporation',
+      Biography:"Renée J. James is currently the chairman and CEO of Ampere Computing, a company she founded in 2017, which produces high-performance semiconductors for hyperscale cloud, storage, and edge computing. James also serves as an operating executive for The Carlyle Group, a global alternative asset manager. In her role with Carlyle, James evaluates new technology investments for the firm as well as advises portfolio companies on their strategic direction and operational efficiency. In 2016, James concluded a 28-year career with Intel Corporation, where she most recently served as president. James is chair of the National Security Telecommunications Advisory Committee to the President of the United States. She also serves as a director of Citigroup Inc. She previously served as a director of Sabre Corporation and Vodafone Group Plc.",
+      source:[
+        {
+          name:"Oracle",
+          link:"https://www.oracle.com/sa/corporate/executives/"
+        }
+      ]
+    },
+
+    {
+      name:'Charles (Wick) Moorman IV',
+      photo:'https://www.fusfoundation.org/images/Moorman_Wick_11.jpg',
+      title:'Senior Advisor to Amtrak and Former CEO of Amtrak and Norfolk Southern Corporation',
+      Biography:"Wick Moorman is currently a senior advisor to Amtrak, where he previously served as president and CEO from August 2016 until January 2018. Prior to that and until 2015, Moorman was CEO (from 2005) and chairman (from 2006) of Norfolk Southern Corporation. From 1975 to 2005, he held various positions in operations, information technology, and human resources at Norfolk Southern Corporation. Moorman serves as a director of Chevron Corporation and previously served as a director of Duke Energy Corporation and Norfolk Southern Corporation.",
+        source:[
+        {
+          name:"Oracle",
+          link:"https://www.oracle.com/sa/corporate/executives/"
+        }
+      ]
+    },
+
+    {
+      name:'Leon E. Panetta',
+      photo:'https://static01.nyt.com/images/2014/10/07/world/PANETTA/PANETTA-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
+      title:'Former Director of the Central Intelligence Agency and Former US Secretary of Defense',
+      Biography:"Leon Panetta served as United States Secretary of Defense from 2011 to 2013 and as Director of the Central Intelligence Agency from 2009 to 2011. Prior to that, Panetta was a member of the United States House of Representatives from 1977 to 1993, served as Director of the Office of Management and Budget from 1993 to 1994, and served as former President Bill Clinton’s Chief of Staff from 1994 to 1997. Panetta is the cofounder and chairman of the Panetta Institute for Public Policy and currently serves as moderator of the Leon Panetta Lecture Series, a program he created. He previously served as Distinguished Scholar to Chancellor Charles B. Reed of the California State University System and professor of public policy at Santa Clara University",
+        source:[
+        {
+          name:"Oracle",
+          link:"https://www.oracle.com/sa/corporate/executives/"
+        }
+      ]
+    },
+
+    {
+      name:'William G. Parrett',
+      photo:'https://www.nopactalent.com/upload/images/speakers/7648/267x267_Screen%20shot%202015-10-01%20at%2012.39.35%20PM.png',
+      title:'Former CEO of Deloitte Touche Tohmatsu',
+      Biography:"William Parrett served as the chief executive officer of Deloitte Touche Tohmatsu, a multinational professional services network, from 2003 until 2007. He joined Deloitte in 1967 and served in a series of roles of increasing responsibility until his retirement in 2007. Parrett serves as a director of The Blackstone Group L.P. and Thoughtworks, Inc. He previously served as a director of the Eastman Kodak Company, Conduent Inc., Thermo Fisher Scientific, UBS Group AG, and IGATE Corporation. Parrett is a Certified Public Accountant with an active license.",
+        source:[
+        {
+          name:"Oracle",
+          link:"https://www.oracle.com/sa/corporate/executives/"
+        }
+      ]
+    },
+
+    {
+      name:'Naomi O. Seligman',
+      photo:'https://cdn.theorg.com/5fee6d32-d4c5-48fd-962b-ed31d5347e6d_small.jpg',
+      title:'Senior Partner, Ostriker von Simson',
+      Biography:"Naomi O. Seligman is a senior partner at Ostriker von Simson, a technology research firm that chairs the CIO Strategy Exchange. Since 1999, this forum has brought together senior executives in four vital quadrants of the IT sector. From 1977 until 1999, Seligman served as a cofounder and senior partner of Research Board, Inc., a private-sector institution sponsored by 100 chief information officers from major global corporations. She previously served as a director of Akamai Technologies, Inc., IGATE Corporation, and Dun & Bradstreet.",
+        source:[
+        {
+          name:"Oracle",
+          link:"https://www.oracle.com/sa/corporate/executives/"
+        }
+      ]
+    },
+
+    {
+      name:'Vishal Sikka',
+      photo:'https://images.itnewsinfo.com/lmi/articles/grande/000000058237.jpg',
+      title:'Founder and CEO of Vianai Systems, Inc.',
+      Biography:"Vishal Sikka is the founder and CEO of Vianai Systems, Inc., a startup company that provides advanced software and services in artificial intelligence and machine learning. Previously, he was the CEO and Managing Director of Infosys Limited, a multinational IT services company, from 2014 to 2017. From 2002 to 2014, Sikka was at SAP SE, a multinational software company, where he served on the Executive Board from 2010 to 2014. He serves on the Supervisory Board of BMW Group and on the Advisory Council for the Stanford Institute for Human-Centered Artificial Intelligence. Sikka holds a PhD in computer science with a focus on artificial intelligence from Stanford University.",
+        source:[
+        {
+          name:"Oracle",
+          link:"https://www.oracle.com/sa/corporate/executives/"
+        }
+      ]
+    },
+
+
+
         ]
       }
 
