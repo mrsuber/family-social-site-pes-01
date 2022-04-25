51cfa2e24460e4d538e225a6b96b785c8fb61061
mohamad <msiysinyuy@gmail.com>
2022-04-25 05:39:21 +0100

    greating data for index card for school'

---
+++ client/src/data/indexPageDisplayData.js
@@ -0,0 +1,15 @@
+import {logo} from '../images'
+export const indexData = [
+  {
+    logo:logo,
+    gs1:"",
+    gs2:"",
+    gs3:"",
+    gs4:"",
+    gs5:"",
+    gs6:"",
+    title:"About Mohamad S.B",
+    page:"blog_home",
+    link:"redirectBlogHomePage"
+  },
+]
