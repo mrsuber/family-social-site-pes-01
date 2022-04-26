bc52164e37b6d97c3856bee88ecc6cdb8fad3f05
mohamad <msiysinyuy@gmail.com>
2022-04-26 13:03:52 +0100

    update on the expense section

--- client/src/components/admin/expense/AdminExpenseForm/AdminExpenseForm.jsx
+++ client/src/components/admin/expense/AdminExpenseForm/AdminExpenseForm.jsx
@@ -9,9 +9,15 @@ const AdminExpenseForm = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-          <Typography align="center" variant="subtitle2" gutterBottom></Typography>
+          <Typography align="center" variant="subtitle2" gutterBottom>
+              ...
+          </Typography>
       </Grid>
-
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+            <InputLabel>Type</InputLabel>
+        </FormControl>
+      </Grid>
     </Grid>
   )
 }
