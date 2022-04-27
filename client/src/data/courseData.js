5938d8d3be9342fdbd38a07e1b5e6d62703f8ea5
mohamad <msiysinyuy@gmail.com>
2022-04-27 12:50:47 +0100

    data layout for foundation done

--- client/src/data/courseData.js
+++ client/src/data/courseData.js
@@ -52,7 +52,7 @@ export const devCourseData = [
 
   },
   {
-    learningTitle:"HTML",
+    learningTitle:"HTML Foundations",
     logo:logo,
     path:"HTML",
     about:"HTML is the universal markup language for the Web. HTML lets you format text, add graphics, create links, input forms, frames and tables, etc., and save it all in a text file that any browser can read and display.",
@@ -90,44 +90,116 @@ export const devCourseData = [
 
   },
   {
-    learningTitle:"HTML/CSS",
+    learningTitle:"CSS Foundations",
     logo:logo,
     path:"CSS",
-    about:"This path takes you through our entire JavaScript curriculum. You'll learn everything you need to know to create beautiful responsive websites from scratch using Html, Css, JavaScript, NodeJs and MongoDb.",
+    about:"CSS stands for Cascading Style Sheets · CSS describes how HTML elements are to be displayed on screen, paper, or in other media ",
     MainLocation:"Cameroon || southwest || Buea",
-    TotalNumberOfBranches:"2",
-    TotalNumberCompleted:"0.0",
+    TotalNumberOfBranches:"9",
+    TotalNumberCompleted:"0",
     LessonTittle:[
       {
-        title:"How this Course Will Work"
+        title:"CSS Foundations"
       },
+      {
+        title:"Inspecting HTML and CSS"
+      },
+      {
+        title:"The Box Model"
+      },
+      {
+        title:"Block and Inline"
+      },
+      {
+        title:"Introduction to Flexbox"
+      },
+      {
+        title:"Growing and Shrinking"
+      },
+      {
+        title:"Axes"
+      },
+      {
+        title:"Alignment"
+      },
+      {
+        title:" Project: Landing Page"
+      },
+
     ],
   },
   {
-    learningTitle:"HTM/CSS/JS",
+    learningTitle:"JavaScript Basics",
     logo:logo,
     path:"JavaScript",
-    about:"This path takes you through our entire JavaScript curriculum. You'll learn everything you need to know to create beautiful responsive websites from scratch using Html, Css, JavaScript, NodeJs and MongoDb.",
+    about:"JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else. (Okay, not everything, but it is amazing what you can achieve with a few lines of JavaScript code.)",
     MainLocation:"Cameroon || southwest || Buea",
-    TotalNumberOfBranches:"2",
-    TotalNumberCompleted:"0.0",
+    TotalNumberOfBranches:"15",
+    TotalNumberCompleted:"0",
     LessonTittle:[
       {
-        title:"How this Course Will Work"
+        title:"Fundamentals Part 1"
       },
+      {
+        title:"Fundamentals Part 2"
+      },
+      {
+        title:"JavaScript Developer Tools"
+      },
+      {
+        title:"Fundamentals Part 3"
+      },
+      {
+        title:"Problem Solving"
+      },
+      {
+        title:"Understanding Errors"
+      },
+      {
+        title:"Project: Rock Paper Scissors"
+      },
+      {
+        title:"Clean Code"
+      },
+      {
+        title:"Installing Node.js"
+      },
+      {
+        title:"Fundamentals Part 4"
+      },
+      {
+        title:"DOM Manipulation and Events"
+      },
+      {
+        title:"Revisiting Rock Paper Scissors"
+      },
+      {
+        title:"Project: Etch-a-Sketch"
+      },
+      {
+        title:"Fundamentals Part 5"
+      },
+      {
+        title:"Project: Calculator"
+      },
+
+      {
+        title:"Conclusion: Choose Your Path Forward"
+      },
+
     ],
   },
   {
     learningTitle:"React",
     logo:logo,
     path:"React FrameWork",
-    about:"This path takes you through our entire JavaScript curriculum. You'll learn everything you need to know to create beautiful responsive websites from scratch using Html, Css, JavaScript, NodeJs and MongoDb.",
+    about:"A framework is a collection of libraries, tools, rules, structures & control used to build software systems. The framework will call your code.React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies.",
     MainLocation:"Cameroon || southwest || Buea",
     TotalNumberOfBranches:"2",
     TotalNumberCompleted:"0.0",
     LessonTittle:[
       {
-        title:"How this Course Will Work"
+        title:"Introduction to Frameworks"
       },
     ],
   },
@@ -141,7 +213,7 @@ export const devCourseData = [
     TotalNumberCompleted:"0.0",
     LessonTittle:[
       {
-        title:"How this Course Will Work"
+        title:"Introduction to the Back End"
       },
     ],
   },
