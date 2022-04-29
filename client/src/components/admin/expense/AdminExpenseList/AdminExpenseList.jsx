edbb98c791ac21543143bf6f959e3f682e8c1b10
mohamad <msiysinyuy@gmail.com>
2022-04-29 08:55:05 +0100

    the update in expense

--- client/src/components/admin/expense/AdminExpenseList/AdminExpenseList.jsx
+++ client/src/components/admin/expense/AdminExpenseList/AdminExpenseList.jsx
@@ -2,11 +2,27 @@ import React from 'react'
 import {List as MUIList,ListItem,ListItemAvatar,ListItemText,Avatar,ListItemSecondaryAction,IconButton,Slide} from '@material-ui/core'
 import {Delete,MoneyOff} from '@material-ui/icons'
 import useStyles from './styles'
+
+
 const AdminExpenseList = () => {
   const classes = useStyles()
+  const transactions = [
+    {id:1,type:"Income",category:"Salary"}
+  ]
   return (
     <MUIList dense={false} className={classes.list}>
-
+        {transactions.map((transaction)=>(
+          <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+              <ListItem>
+                  <ListItemAvatar>
+                      <Avatar className={transaction.type === 'Income'? classes.avatarIncome : classes.avatarExpense}>
+                        <MoneyOff />
+                      </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={transaction.category}/>
+              </ListItem>
+          </Slide>
+        ))}
     </MUIList>
   )
 }
