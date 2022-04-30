71a30edb13b5bc00c77c9ad169fa7cb28522a1cc
mohamad <msiysinyuy@gmail.com>
2022-04-30 09:30:09 +0100

    i update the Git setup learning data

--- client/src/data/courseStructuralData.js
+++ client/src/data/courseStructuralData.js
@@ -1782,13 +1782,48 @@ export const courseStructuralData = [
             detail6:"",
 
             ancorName:"Assignment",
-            popup1Name:"",
-            popup1:"",
+            popup1Name:"setting up first git and github repo",
+            popup1:"https://res.cloudinary.com/msb-geneasocial/image/upload/v1651305390/msbGeanologyProfilePics/creating_repo_mljkrl.png",
             // popup2Name:"",
             // popup2:"",
 
 
           },
+
+              {
+                name:'Note/warning',
+                detail:"When trying to make simple changes to the files in your repo, such as attempting to fix a typo in your README.md you might be tempted to make this change directly via Github. However, it is best to avoid this as it will cause issues that require more advanced Git knowledge than we want to go over at this stage (it is covered in a future lesson), for now it is advised to make any changes via your local files then commit and push them using Git commands in your terminal once ready.",
+                detail1:"",
+                detail2:"",
+                detail3:"",
+                detail4:"",
+                detail5:"",
+                detail6:"",
+                ancorName:"Note/warning",
+                popup1Name:"",
+                popup1:"",
+                popup2Name:"",
+                popup2:"",
+
+
+              },
+              {
+                name:'Cheatsheet',
+                detail:"This is a reference list of the most commonly used Git commands. (You might consider bookmarking this handy page.) Try to familiarize yourself with the commands so that you can eventually remember them all:",
+                detail1:"Commands related to a remote repository: ==> git clone git@github.com:USER-NAME/REPOSITORY-NAME.git",
+                detail2:"==>git push or git push origin main (Both accomplish the same goal in this context)",
+                detail3:"Commands related to the workflow:==>git add .",
+                detail4:'==>git commit -m "A message describing what you have done to make this snapshot different"',
+                detail5:"Commands related to checking status or log history ==> git status ==>git log",
+                detail6:'The basic Git syntax is program | action | destination. For example, ==>1 git add . is read as git | add | ., where the period represents everything in the current directory; ==>2 git commit -m "message" is read as git | commit -m | "message"; and ==>3. git status is read as git | status | (no destination).',
+                ancorName:"Cheatsheet",
+                popup1Name:"",
+                popup1:"",
+                popup2Name:"",
+                popup2:"",
+
+
+              },
           {
             name:'Git Best Practices',
             detail:"There’s a lot to learn about using Git. But it is worth taking the time to highlight some best practices so that you can be a better collaborator. Git is not only helpful when collaborating with others. It’s also useful when working independently. You will be relying more and more on your own commit history in the future when revisiting old code.",
