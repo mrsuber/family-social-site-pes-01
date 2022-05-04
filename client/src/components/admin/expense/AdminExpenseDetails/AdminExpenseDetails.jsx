b3314b7ea68ec7a767abf8a3c65ae1a26f4f150a
mohamad <msiysinyuy@gmail.com>
2022-05-04 14:57:15 +0100

    the chart data representation done

--- client/src/components/admin/expense/AdminExpenseDetails/AdminExpenseDetails.jsx
+++ client/src/components/admin/expense/AdminExpenseDetails/AdminExpenseDetails.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import {Card, CardHeader,CardContent,Typography} from '@material-ui/core'
+import 'chart.js/auto';
 import {Doughnut} from 'react-chartjs-2'
+import useTransactions from '../../../../costomHooks/useTransactions'
 
 import useStyles from './styles'
 
 const AdminExpenseDetails = ({title}) => {
   const classes = useStyles();
+  const {total, chartData} = useTransactions(title)
+  console.log("chartData",chartData)
   return (
     <Card className={title === 'Income' ? classes.income : classes.expense}>
       <CardHeader title={title}/>
       <CardContent>
-        <Typography variant="h5">$50</Typography>
-
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData}/>
       </CardContent>
 
     </Card>
