0499f2d073d9aaf74b7ae53427463e2b46ad7127
mohamad <msiysinyuy@gmail.com>
2022-05-08 15:44:36 +0100

    the expense updated to usable for now

---
+++ client/src/data/expenseData.js
@@ -0,0 +1,17 @@
+export const allExpenseData = [
+  {
+    amount:'28.57',
+    category:'Gifts',
+    type:'Income',
+    details:'Gift form Mr Mosses',
+    date: '05-07-2022',
+  },
+  {
+    amount:'0.81',
+    category:'Food',
+    type:'Expense',
+    details:'bought potatoes for 500frs or $0.81',
+    date: '05-08-2022',
+  }
+
+]
