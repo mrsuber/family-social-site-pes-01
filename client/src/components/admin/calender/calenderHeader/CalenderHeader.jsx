9e286f978fb74980960ae37c676012b8a94a9b01
mrsuber <msiysinyuy@gmail.com>
2022-06-27 17:56:42 +0100

    update on the calender

--- client/src/components/admin/calender/calenderHeader/CalenderHeader.jsx
+++ client/src/components/admin/calender/calenderHeader/CalenderHeader.jsx
@@ -5,6 +5,12 @@ const CalenderHeader = ({data}) =>{
   return(
     <div className="CalenderHeader__wrapper">
       <img src={data.logo} alt="logo" className=" CalenderHeader__logo"/>
+      <h1 className="CalenderHeader__heading">Calendar</h1>
+      <button className="CalenderHeader__button">Today</button>
+      <button className="CalenderHeader__button2">
+        <span></span>
+        <span></span>
+      </button>
     </div>
   )
 }
