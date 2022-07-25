2cf188d1136f817d2893efa07685ff6463ad56d1
mrsuber <msiysinyuy@gmail.com>
2022-07-25 20:27:06 +0100

    update on the input ui of the create event

--- client/src/components/admin/calender/calenderEventModal/CalenderEventModal.js
+++ client/src/components/admin/calender/calenderEventModal/CalenderEventModal.js
@@ -1,17 +1,31 @@
 
-import {useContext} from 'react'
+import {useContext,useState} from 'react'
 import './CalenderEventModal.css'
 import GlobalContext from '../../../../context/GlobalContext'
 
 const CalenderEventModal = () =>{
   const {setShowEventModal} = useContext(GlobalContext)
-
+  const [title,setTitle] = useState('')
   return(
     <div className="eventModal">
       <form className="eventModal__form">
         <div className="eventModal__header">
           <span className="eventModal__span">-</span>
           <button onClick={()=>setShowEventModal(false)}>close</button>
+        </div>
+        <div className="eventModal__formbody">
+          <div className="eventModal__formbodychild">
+            <div></div>
+            <input
+            type="text"
+            name="title"
+            placeholder="Add Tittle"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
+            required
+            className="eventModal__formInput"
+            />
+          </div>
         </div>
       </form>
     </div>
