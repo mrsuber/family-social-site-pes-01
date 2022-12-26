7e3b1b5e5b6a6e9daac1ecabf35126d3f16594bf
Mohamad <msiysinyuy@gmail.com>
2022-12-26 21:35:45 +0100

    update to calender

--- client/src/components/admin/calender/calenderDay/CalenderDay.jsx
+++ client/src/components/admin/calender/calenderDay/CalenderDay.jsx
@@ -1,12 +1,17 @@
 import './CalenderDay.css'
 import dayjs from "dayjs"
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import GlobalContext from '../../../../context/GlobalContext'
 const CalenderDay = ({day,rowIdx})=>{
+  const [dayEvents, setDayEvents] = useState([])
+  const {setDaySelected, setShowEventModal,savedEvents,setSelectedEvent} = useContext(GlobalContext)
+  useEffect(()=>{
+    const events = savedEvents.filter(evt => dayjs(evt.day).format("DD-MM-YY") === day.format("DD-MM-YY"))
+    setDayEvents(events)
+  },[savedEvents,day])
   function getCurrentDayClass(){
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? "calenderDay__active":''
   }
-  const {setDaySelected, setShowEventModal} = useContext(GlobalContext)
   return(
     <div className="calenderDay__wrapper">
     <div className="calenderDay__header">
@@ -14,12 +19,22 @@ const CalenderDay = ({day,rowIdx})=>{
         <p className="calenderDay__text2">{day.format('ddd').toUpperCase()}</p>
       )}
       <p className={`calenderDay__text ${getCurrentDayClass()}`}>{day.format('DD')}</p>
+      
     </div>
     <div className='calenderDay__wrapper2' onClick={()=>{
       setDaySelected(day)
       setShowEventModal(true)
     }}>
-          {"dd"}
+          {dayEvents.map((evt,idx)=>(
+            <div 
+              key={idx}
+              style={{background:`${evt.label}`}}
+              className="calenderDay__event"
+              onClick={()=>setSelectedEvent(evt)}
+            >
+              {evt.title}
+            </div>
+          ))}
     </div>
 
     </div>
